eba6df5436391ee5500ee946d5d7638892ecda14
jmusila <jonathanmusila6@gmail.com>
2021-09-28 14:45:05 +0300

    added necessary methods

--- src/Weight.php
+++ src/Weight.php
@@ -4,4 +4,25 @@ namespace Jmusila\UnitConversions;
 
 class Weight
 {
+    private float $kilograms;
+
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.2046;
+    }
+
+    public function toTons(): float
+    {
+        return $this->kilograms / 1000.0;
+    }
 }
